c848e9699fb9d0d432d1d83e1debd85382a69f98
camilogonzalezf <camilogonzalezf8@gmail.com>
2023-10-08 10:25:49 -0500

    add text in second path

--- resources/views/second.blade.php
+++ resources/views/second.blade.php
@@ -10,6 +10,8 @@
 
 <body>
     <h1>Second route</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere rerum natus odit illo odio, consequatur quod
+        sint repudiandae! Itaque accusantium optio aspernatur tempore sit beatae? Qui impedit libero aut sint.</p>
 </body>
 
 </html>
